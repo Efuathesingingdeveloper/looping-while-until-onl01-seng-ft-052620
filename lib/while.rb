f0e9aa320c3a58f7de5d7006fcd9d5b5bc2e21c4
Georgette Class-Peters <georgycp@udel.edu>
2020-06-02 22:21:05 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,12 @@
 def using_while
   levitation_force = 6
-	
+	puts ""
   #your code here
 end
 
-
+describe "#using_while" do 
+	it "puts out the phrase 'Wingardium Leviosa' while the levitation force is incremented to 10" do 
+	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	expect{ using_while }.to output(looping_string).to_stdout
+	end
+end
